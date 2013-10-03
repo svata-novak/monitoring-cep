9f19355df56518910ade18bbcf7de5dfea868b7e
xtovarn <dan.tovarnak@gmail.com>
2013-10-03 16:08:10 +0200

    * refactor

--- predicates-erl/src/seal_boolfuns.erl
+++ predicates-erl/src/seal_boolfuns.erl
@@ -1,40 +1,34 @@
--module(functions).
+-module(seal_boolfuns).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([projection/2, eq/2, neq/2, eq2/2, neq2/2, gt/2, gte/2, lt/2,
-		 lte/2]).
+-compile(export_all).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
-eq(Val1, Val2) ->
+eq({Val1, Val2}) ->
 	Val1 =:= Val2.
 
-neq(Val1, Val2) ->
+neq({Val1, Val2}) ->
 	Val1 =/= Val2.
 
-eq2(Val1, Val2) ->
+eq2({Val1, Val2}) ->
 	Val1 == Val2.
 
-neq2(Val1, Val2) ->
+neq2({Val1, Val2}) ->
 	Val1 /= Val2.
 
-gt(Val1, Val2) ->
+gt({Val1, Val2}) ->
 	Val1 > Val2.
 
-gte(Val1, Val2) ->
+gte({Val1, Val2}) ->
 	Val1 >= Val2.
 
-lt(Val1, Val2) ->
+lt({Val1, Val2}) ->
 	Val1 < Val2.
 
-lte(Val1, Val2) ->
+lte({Val1, Val2}) ->
 	Val1 =< Val2.
-
-projection(Event, Attribute) when is_list(Attribute) ->
-	event:fetch(Attribute, Event).
-
-%%projection(Attribute) when is_list(Attribute) ->
--- predicates-erl/src/seal_dan_predicates.erl
+++ predicates-erl/src/seal_dan_predicates.erl
@@ -9,53 +9,29 @@
 -module(seal_dan_predicates).
 -author("xtovarn").
 
--record(disjunction, {terms = []}).
+-record(clause, {terms = []}).
 -record(predicate, {disjunctions = []}).
 
 %% API
--export([eval/2, projection/2, eq/1, neq/1, eq2/1, neq2/1, gt/1, gte/1, lt/1, lte/1]).
+-export([eval/2]).
 
 eval(Predicate = #predicate{}, Events) ->
-	lists:all(fun(Disjunction = #disjunction{}) -> eval(Disjunction, Events) end, Predicate#predicate.disjunctions);
-eval(Disjunction = #disjunction{}, Events) ->
+	lists:all(fun(Disjunction = #clause{}) -> eval(Disjunction, Events) end, Predicate#predicate.disjunctions);
+eval(Clause = #clause{}, Events) ->
 	lists:any(
-		fun(Term) ->
-			{Fun, Args} = Term,
-			?MODULE:Fun(args(Args, Events))
+		fun(Literal) ->
+			{Fun, Args} = Literal,
+			seal_boolfuns:Fun(args(Args, Events))
 		end,
-		Disjunction#disjunction.terms).
-
-eq({Val1, Val2}) ->
-	Val1 =:= Val2.
-
-neq({Val1, Val2}) ->
-	Val1 =/= Val2.
-
-eq2({Val1, Val2}) ->
-	Val1 == Val2.
-
-neq2({Val1, Val2}) ->
-	Val1 /= Val2.
-
-gt({Val1, Val2}) ->
-	Val1 > Val2.
-
-gte({Val1, Val2}) ->
-	Val1 >= Val2.
-
-lt({Val1, Val2}) ->
-	Val1 < Val2.
-
-lte({Val1, Val2}) ->
-	Val1 =< Val2.
-
-arg({Fun, [{event, Number}, Attribute]}, Events) ->
-	?MODULE:Fun(lists:nth(Number, Events), Attribute);
-arg({constant, Constant}, _Events) ->
-	Constant.
+		Clause#clause.terms).
 
 args([First, Second], Events) ->
 	{arg(First, Events), arg(Second, Events)}.
+
+arg({projection, [{event, Number}, Attribute]}, Events) ->
+	projection(lists:nth(Number, Events), Attribute);
+arg({constant, Constant}, _Events) ->
+	Constant.
 
 projection(Event, Attribute) when is_list(Attribute) ->
 	event:fetch(Attribute, Event).
